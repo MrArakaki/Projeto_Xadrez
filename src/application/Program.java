106a15546467aca84676eb701fc53d9563a77f42
Marcelo Ribeiro <arakaki2k2@gmail.com>
2020-01-30 12:09:12 -0300

    projeto xadrez

---
+++ src/application/Program.java
@@ -0,0 +1,21 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		// jogo de xadrez- 30/01/2020
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		sc.close();
+
+	}
+
+}
