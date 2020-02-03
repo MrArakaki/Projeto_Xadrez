916020de7607cbfcf95ec7152ad5ba7792c303e9
Marcelo Ribeiro <arakaki2k2@gmail.com>
2020-02-03 07:18:50 -0300

    projeto xadrez

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,8 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.Rook;
 
 // contém as regras do jogo.
 
@@ -10,20 +12,29 @@ public class ChessMatch {
 
 	public ChessMatch() {
 
-		board = new Board(8, 8); // dimensão do tabulheiro
+		board = new Board(8,8); // dimensão do tabulheiro
+		initialSetup();
 	}
 // metodo para retornar uma matriz de peças da partida
 
 	public ChessPiece[][] getPieces() {
 		// variavel temporária auxiliar
 		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
+
 		// precorre a matriz
 		for (int i = 0; i < board.getRows(); i++) {
-			for (int j = 0; j < board.getColumns(); i++) {// precorre a coluna
-				mat[i][j]=(ChessPiece) board.piece(i,j); // drawcast
+			// precorre a coluna
+			for (int j = 0; j < board.getColumns(); i++) {
 
+				mat[i][j] = (ChessPiece) board.piece(i, j); // drawcast em chessPiece
 			}
+
 		}
 		return mat;
+	}
+
+	private void initialSetup() {
+		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
+
 	}
 }
